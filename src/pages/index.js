690543c97e80c22113519687e4e2c3e03d764211
Dejan Malidza <dejanmalidza@gmail.com>
2023-03-28 09:39:01 +0200

    CommitTry

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,6 @@ import Poll from 'mdi-material-ui/Poll'
 import CurrencyUsd from 'mdi-material-ui/CurrencyUsd'
 import HelpCircleOutline from 'mdi-material-ui/HelpCircleOutline'
 import BriefcaseVariantOutline from 'mdi-material-ui/BriefcaseVariantOutline'
-
 // ** Custom Components Imports
 import CardStatisticsVerticalComponent from 'src/@core/components/card-statistics/card-stats-vertical'
 
