d9a72e58c87aab5fad713b88107566294c11dc58
Dejan Malidza <dejanmalidza@gmail.com>
2023-03-29 10:35:38 +0200

    Date

--- src/pages/trademarks/index.js
+++ src/pages/trademarks/index.js
@@ -12,19 +12,40 @@ import TableSpanning from 'src/views/tables/TableSpanning'
 import TableCustomized from 'src/views/tables/TableCustomized'
 import TableCollapsible from 'src/views/tables/TableCollapsible'
 import TableStickyHeader from 'src/views/tables/TableStickyHeader'
+import styled from '@emotion/styled'
+import DatePicker from 'react-datepicker'
+import React, { useState } from 'react'
+import 'react-datepicker/dist/react-datepicker.css'
+
+const WrapperDate = styled.div`
+  padding: 15px 0 15px 0;
+  display: inline-flex;
+`
+
+const DatePicker1 = styled(DatePicker)`
+  width: 130px;
+  margin-right: 10px;
+  }
+`
 
 const MUITable = () => {
+  const [startDate, setStartDate] = useState(new Date())
   return (
     <Grid container spacing={6}>
       <Grid item xs={12}>
-        <Typography variant='h5'>
+        <Typography variant='h5' style={{ borderBottom: '2px solid black' }}>
           <Link href='https://mui.com/components/tables/' target='_blank' style={{ color: 'black' }}>
-            MUI Tables
+            Trademarks List
           </Link>
         </Typography>
-        <Typography variant='body2' style={{ color: 'black' }}>
-          Tables display sets of data. They can be fully customized
-        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <WrapperDate>
+          <p style={{ margin: '0', marginRight: '5px', color: 'black' }}>From:</p>
+          <DatePicker1 selected={startDate} onChange={date => setStartDate(date)} />
+          <p style={{ margin: '0', marginRight: '5px', color: 'black' }}>To:</p>
+          <DatePicker1 selected={startDate} onChange={date => setStartDate(date)} />
+        </WrapperDate>
       </Grid>
       <Grid item xs={12}>
         <Card>
